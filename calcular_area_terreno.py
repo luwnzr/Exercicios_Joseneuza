4763bf18e6ca83c56b2c56b74264102fbb15398a
Luane Gabrielly <luanegabrielly2202@gmail.com>
2024-11-04 16:53:54 -0300

    Create calcular_area_terreno.py

---
+++ calcular_area_terreno.py
@@ -0,0 +1,6 @@
+”””Criar um programa em python que leia os valores de largura e comprimento em metros e calcule a area do terreno area_do_terreno = largura * comprimento”””
+
+largura = float(input(“Informe a largura do terreno))
+comprimento = float(input(“Informe o comprimento do terreno))
+area = largura * comprimento
+print(“area do terreno é”, area)

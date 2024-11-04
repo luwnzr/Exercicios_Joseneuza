216bd6cd1c7baa6dde441b9b3ceb67c0d1e023e0
Luane Gabrielly <luanegabrielly2202@gmail.com>
2024-11-04 16:53:31 -0300

    Create calcular_area_triangulo.py

---
+++ calcular_area_triangulo.py
@@ -0,0 +1,8 @@
+”””Criar um programa python que leia a base e altura e calcule a área do triangulo”””
+
+
+base = float(input(“Informe o valor da base”))
+altura = float(input(“Informe o valor da altura”))
+area = (base * altura) / 2
+
+print(“Area do Triangulo”, area)

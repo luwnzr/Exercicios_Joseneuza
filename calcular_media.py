31510f16227a3edf05f406f207724d6bf7a0c0d7
Luane Gabrielly <luanegabrielly2202@gmail.com>
2024-11-04 15:02:37 -0300

    Create calcular_media.py

---
+++ calcular_media.py
@@ -0,0 +1,7 @@
+"""Criar um programa em python que leia o nome, duas notas e calcule a media aritmética ao final exiba o nome do aluno e a média das notas"""
+
+nome = input("Digite seu nome ")
+nota1 = float(input("Digite a primeira nota"))
+nota2 = float(input("Digite a segunda nota"))
+media = (nota1=nota2) / 2
+print("Aluno(a) ", nome, "obteve média final ", media)

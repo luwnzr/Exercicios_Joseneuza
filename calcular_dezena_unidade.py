f5f54bed6ada002aae10aac5dcc4f98d9129b05e
Luane Gabrielly <luanegabrielly2202@gmail.com>
2024-11-04 17:10:20 -0300

    Create calcular_dezena_unidade.py

---
+++ calcular_dezena_unidade.py
@@ -0,0 +1,8 @@
+"""Criar um programa em python que leia um número até 99, dezena e unidade do numero lido: Ex. ler 35 deverá exibir 5 unidades"""
+
+numerp = int(input("Digite um numero ate 99"))
+dezena = numero // 10
+unidade = numero % 10
+
+print("Dezena "=, dezena")
+print("Unidade "= unidade)

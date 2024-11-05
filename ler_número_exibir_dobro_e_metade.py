20bfd3a45c2a94316de57d8f27fe72122e9eb952
Luane Gabrielly <luanegabrielly2202@gmail.com>
2024-11-05 16:49:28 -0300

    Create ler_número_exibir_dobro_e_metade.py

---
+++ ler_número_exibir_dobro_e_metade.py
@@ -0,0 +1,8 @@
+"""Criar um programa em python que leia um numero e exiba o seu dobro e a sua metade"""
+
+numero = float(input("Digite um número: "))
+dobro = numero * 2
+metade = numero / 2
+
+print(f"O dobro de {numero} é {dobro}")
+print(f"A metade de {numero} é {metade}")
